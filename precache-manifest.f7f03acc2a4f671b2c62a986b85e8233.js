9b219a296c1b44482e215d8f9cc9ff06cbd8989a
unknown <syyadkhan@yahoo.com>
2018-10-24 20:47:00 -0400

    Updates

---
+++ precache-manifest.f7f03acc2a4f671b2c62a986b85e8233.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "64e7d500b698b8e49ad9",
+    "url": "/react-materialui-training-app/static/js/runtime~main.64e7d500.js"
+  },
+  {
+    "revision": "3e161a9dff7467df61e4",
+    "url": "/react-materialui-training-app/static/js/main.3e161a9d.chunk.js"
+  },
+  {
+    "revision": "9cf8228de4e02bed9ae9",
+    "url": "/react-materialui-training-app/static/js/1.9cf8228d.chunk.js"
+  },
+  {
+    "revision": "3e161a9dff7467df61e4",
+    "url": "/react-materialui-training-app/static/css/main.6794d923.chunk.css"
+  },
+  {
+    "revision": "a50a6accc072a79a0ec85423c7043ea6",
+    "url": "/react-materialui-training-app/index.html"
+  }
+];
