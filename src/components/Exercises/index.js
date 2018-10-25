e4507276b6a0fb9f394763ab77059abe2eee65de
unknown <syyadkhan@yahoo.com>
2018-10-24 20:20:29 -0400

    final great Material ui  project

--- src/components/Exercises/index.js
+++ src/components/Exercises/index.js
@@ -1,52 +1,78 @@
 import React, { Fragment } from "react";
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
-import IconButton from "@material-ui/core/IconButton";
-import DeleteIcon from "@material-ui/icons/Delete";
-import EditIcon from "@material-ui/icons/Edit";
+import {
+  Grid,
+  Paper,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton
+} from "@material-ui/core";
+import { Edit, Delete } from "@material-ui/icons";
+import { withStyles } from "@material-ui/core/styles";
 import Form from "./Form";
 
-const styles = {
-  Paper: {
-    padding: 20,
-    marginTop: 5,
-    height: 500,
-    overflowY: "auto"
-  }
-};
-
-export default function index({
-  muscles,
-  exercises,
-  category,
-  editMode,
-  onSelect,
-  exercise,
-  exercise: {
-    id,
-    title = "Welcome",
-    description = "Please select an item from the list on the left."
+const styles = theme => ({
+  paper: {
+    padding: theme.spacing.unit * 3,
+    overflowY: "auto",
+    [theme.breakpoints.up("sm")]: {
+      marginTop: 5,
+      height: "calc(100% - 10px)"
+    },
+    [theme.breakpoints.down("xs")]: {
+      height: "100%"
+    }
   },
-  onDelete,
-  onSelectEdit,
-  onEdit
-}) {
-  return (
-    <Grid container>
-      <Grid item xs={12} sm={6}>
-        <Paper style={styles.Paper}>
+  "@global": {
+    "html, body, #root": {
+      height: "100%"
+    }
+  },
+  container: {
+    [theme.breakpoints.up("sm")]: {
+      height: "calc(100% - 64px - 48px)"
+    },
+    [theme.breakpoints.down("xs")]: {
+      height: "calc(100% - 56px - 48px)"
+    }
+  },
+  item: {
+    [theme.breakpoints.down("xs")]: {
+      height: "50%"
+    }
+  }
+});
+
+export default withStyles(styles)(
+  ({
+    classes,
+    muscles,
+    exercises,
+    category,
+    editMode,
+    onSelect,
+    exercise,
+    exercise: {
+      id,
+      title = "Welcome!",
+      description = "Please select an exercise from the list on the left."
+    },
+    onDelete,
+    onSelectEdit,
+    onEdit
+  }) => (
+    <Grid container className={classes.container}>
+      <Grid item className={classes.item} xs={12} sm={6}>
+        <Paper className={classes.paper}>
           {exercises.map(
             ([group, exercises]) =>
               !category || category === group ? (
                 <Fragment key={group}>
                   <Typography
-                    varient="headline"
                     color="secondary"
+                    variant="headline"
                     style={{ textTransform: "capitalize" }}
                   >
                     {group}
@@ -60,13 +86,13 @@ export default function index({
                             color="primary"
                             onClick={() => onSelectEdit(id)}
                           >
-                            <EditIcon />
+                            <Edit />
                           </IconButton>
                           <IconButton
                             color="primary"
                             onClick={() => onDelete(id)}
                           >
-                            <DeleteIcon />
+                            <Delete />
                           </IconButton>
                         </ListItemSecondaryAction>
                       </ListItem>
@@ -77,9 +103,9 @@ export default function index({
           )}
         </Paper>
       </Grid>
-      <Grid item xs={12} sm={6}>
-        <Paper style={styles.Paper}>
-          <Typography variant="display1" gutterBottom color="secondary">
+      <Grid item className={classes.item} xs={12} sm={6}>
+        <Paper className={classes.paper}>
+          <Typography gutterBottom variant="display1" color="secondary">
             {title}
           </Typography>
           {editMode ? (
@@ -95,5 +121,5 @@ export default function index({
         </Paper>
       </Grid>
     </Grid>
-  );
-}
+  )
+);
